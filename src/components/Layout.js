2199c9caf167a03fa9d80aa184a814756ad64b22
Kenneth Chen <efeichen@gmail.com>
2020-11-20 23:15:33 -0800

    Remove GlobalStyle in layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
 
 import Header from './Header';
 import Footer from './Footer';
@@ -7,18 +6,11 @@ import Footer from './Footer';
 const Layout = ({ children }) => {
     return (
         <>
-            <GlobalStyle />
             <Header />
             <main>{children}</main>
             <Footer />
         </>
     );
 };
-
-const GlobalStyle = createGlobalStyle`
-    body {
-        font-family: 'Trap', sans-serif;
-    }
-`;
 
 export default Layout;
