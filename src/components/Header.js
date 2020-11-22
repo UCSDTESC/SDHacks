5310400e7a9eebc8dbec81c43a33fc3a74f24897
Kenneth Chen <efeichen@gmail.com>
2020-11-22 00:59:36 -0800

    Implement header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'styled-components/macro';
 
 const Header = () => {
-    return <header>Header!</header>;
+    return (
+        <Navbar
+            bg="orange-light"
+            expand="lg"
+            css={`
+                font-weight: 900;
+
+                .navbar-brand span {
+                    color: var(--dark);
+                }
+
+                .navbar-nav {
+                    margin-right: 9rem;
+                    text-transform: uppercase;
+                }
+
+                .nav-link {
+                    transition: color 0.1s;
+                }
+            `}
+        >
+            <Container>
+                <Navbar.Brand href="#">
+                    SD
+                    <span>hacks</span>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar-nav" />
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createGlobalStyle } from 'styled-components';
 
 import Routes from './Routes';
 import './scss/main.scss';
 
-const GlobalStyle = createGlobalStyle`
-    body {
-        font-family: 'Trap', sans-serif;
-    }
-`;
-
 ReactDOM.render(
     <>
-        <GlobalStyle />
         <BrowserRouter>
             <Routes />
         </BrowserRouter>
