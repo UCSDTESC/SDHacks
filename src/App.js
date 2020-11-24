4bec0b8477ad83a88631ce7d16593a45ae83abbd
Kenneth Chen <efeichen@gmail.com>
2020-11-23 20:46:19 -0800

    Init about section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
 import Hero from './components/Hero';
+import About from './components/About';
 
 class SDHacksPage extends Component {
     render() {
         return (
             <>
                 <Hero />
+                <About />
             </>
         );
     }
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const About = () => {
+    return (
+        <section>
+            <Container>Write your about page in here!</Container>
+        </section>
+    );
+};
+
+export default About;
