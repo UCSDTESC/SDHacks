f460ad7f1c371b2bea4a2f0f73a16ba2d0571794
Kenneth Chen <efeichen@gmail.com>
2020-11-25 23:38:34 -0800

    Update about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,55 +3,34 @@ import { Container, Row, Col } from 'react-bootstrap';
 import styled from 'styled-components';
 
 const About = () => {
-    const AboutSection = styled.section`
-    background-color: #393744;
-    height: auto;
-    width: 100%;
-    padding-bottom: 10rem;
-    @media-query (max-width: 1200px) {
-        height: 50%;
-    }
-`
-    const StyledContainer = styled(Container)`
-    color: #FFFFFF;
-    height: 30rem;
-    width: 100%;
-    padding: 6% 10% 0 5%;
-    font-family: Trap;
-`
-    const Title = styled.h1`
-    padding-left: 1rem;
-    font-weight: 900;
-    font-size: 60px;
-    line-height: 66px;
-    
-`
-    const Description = styled.p`
-    font-weight: 300;
-    font-size: 1em;
-    line-height: 160%;
-`
-    const aboutStyle = {
-        color: '#d66440',
-        fontWeight: '900',
-        fontSize: '1em',
-    }
-
     return (
         <AboutSection>
             <StyledContainer>
                 <Row>
-                    <Col>
+                    <Col xl={5} lg={6}>
                         <Title>
-                            <h1 style={aboutStyle}>about</h1>
+                            <span>about</span>
+                            <br />
                             SDHACKS
                         </Title>
                     </Col>
-                    <Col xs={8}>
+                    <Col>
                         <Description>
-                            SD Hacks has been one of the largest hackathons in California since its inception back in 2015. We take pride in being a fully student organized event, while partnering with numerous other student-run engineering organizations at UC San Diego.
-                            <br/><br/>
-                            SD Hacks 2021 will provide the support and mentorship necessary to develop innovative software projects. With mentorship coming from a variety of engineering disciplines and companies, SD Hacks 2021 is shaping up to become an unforgettable 36 hours of collaboration, empowerment, and innovation. This year, SD Hacks will be going completely virtual and bringing the hackathon to you.
+                            SD Hacks has been one of the largest hackathons in
+                            California since its inception back in 2015. We take
+                            pride in being a fully student organized event,
+                            while partnering with numerous other student-run
+                            engineering organizations at UC San Diego.
+                        </Description>
+                        <Description>
+                            SD Hacks 2021 will provide the support and
+                            mentorship necessary to develop innovative software
+                            projects. With mentorship coming from a variety of
+                            engineering disciplines and companies, SD Hacks 2021
+                            is shaping up to become an unforgettable 36 hours of
+                            collaboration, empowerment, and innovation. This
+                            year, SD Hacks will be going completely virtual and
+                            bringing the hackathon to you.
                         </Description>
                     </Col>
                 </Row>
@@ -59,5 +38,35 @@ const About = () => {
         </AboutSection>
     );
 };
+
+const AboutSection = styled.section`
+    background-color: #393744;
+`;
+
+const StyledContainer = styled(Container)`
+    color: white;
+    width: 100%;
+    padding: 5rem 0;
+    font-family: Trap, sans-serif;
+`;
+
+const Title = styled.h1`
+    font-weight: 900;
+    font-size: clamp(2rem, 15vw, 5rem);
+    margin-bottom: 2rem;
+
+    span {
+        color: var(--orange);
+        font-weight: 900;
+        font-size: 1em;
+    }
+`;
+
+const Description = styled.p`
+    font-weight: 300;
+    font-size: 1em;
+    line-height: 160%;
+    margin-bottom: 2rem;
+`;
 
 export default About;
