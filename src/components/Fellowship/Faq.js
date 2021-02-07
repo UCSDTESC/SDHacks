fcb91dea5bca29144fcc4be34ffe218cf956b35c
LiLronV <36945156+LiLronV@users.noreply.github.com>
2021-02-06 18:40:27 -0800

    Created FAQ and About pages (#11)
    * Created FAQ and About pages
    
    * Remove Y padding for consistency
    
    Co-authored-by: Kenneth Chen <efeichen@gmail.com>

---
+++ src/components/Fellowship/Faq.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+import styled from 'styled-components';
+import 'styled-components/macro';
+
+const Faq = () => {
+    return (
+        <FaqContainer>
+            <Container>
+                <h1
+                    style={{
+                        fontWeight: '900',
+                        fontSize: 'clamp(2rem, 15vw, 5rem)',
+                    }}
+                >
+                    Frequently Asked Questions
+                </h1>
+                <Row md={1} sm={1}>
+                    {FaqData.map((d, i) => (
+                        <Col key={i} className="my-3">
+                            <div>
+                                <h2>{d.question}</h2>
+                                <p>{d.answer}</p>
+                            </div>
+                        </Col>
+                    ))}
+
+                    <Col className="my-3">
+                        <div className="questionAnswer">
+                            <h2>More questions?</h2>
+                            <p>
+                                Contact us at{' '}
+                                <a href="mailto:sdhacks@tesc.ucsd.edu">
+                                    sdhacks@tesc.ucsd.edu
+                                </a>
+                            </p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </FaqContainer>
+    );
+};
+const FaqData = [
+    {
+        question: "Who's the Fellowship for?",
+        answer:
+            "SD Hacks Fellowship serves those interested in continuing their SD Hacks projects as something bigger - either startups, non-profits, or impactful side-projects. (But you don't have to strictly stick with your idea from SD Hacks)..",
+    },
+    {
+        question: "What's the application process like?",
+        answer:
+            'One written application and one 30 mins interview. Application opens on February 17th, 2020, and closes on February 23rd, 2020.',
+    },
+    {
+        question: "Who's eligible to apply?",
+        answer:
+            'An applicant needs to be a team of 2 - 4 UCSD students. At least one of the team members needs to be a participant in SD Hacks 2021. Both undergrad and grad students are welcome.',
+    },
+    {
+        question: 'What do you look for in the applicants?',
+        answer:
+            "Make sure you really know the problem you're going to tackle as well as the technical skillsets that you'll need. We like applicants who truly care about the problem of their focus and who are confident in their technical abilities",
+    },
+    {
+        question: 'Do I need previous statup experience?',
+        answer: 'No.',
+    },
+    {
+        question: 'How many Fellows do you select?',
+        answer: '10 - 20 students for SD Hacks Fellowship 2021.',
+    },
+    {
+        question: 'What is the expected time commitment?',
+        answer:
+            "8+ hrs/week. You're going to attend 1 or 2 workshops and/or speaker events every week. SD Hacks 2021 will kick off on March 8th and ends on May 17th..",
+    },
+    {
+        question: 'Do you take equity?',
+        answer: 'No. Our only interest is you.',
+    },
+];
+
+const FaqContainer = styled.div`
+    background-color: var(--orange-light);
+    color: var(--dark);
+    padding: 5% 0;
+
+    a {
+        font-weight: bold;
+        color: var(--dark);
+    }
+
+    h1 {
+        text-align: center;
+        margin-bottom: 2%;
+    }
+
+    h2 {
+        color: var(--gray);
+    }
+
+    p {
+        font-size: 80%;
+    }
+`;
+export default Faq;
--- src/pages/Fellowship.js
+++ src/pages/Fellowship.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import Provide from '../components/Fellowship/Provide';
 import Sponsor from '../components/Fellowship/Sponsor';
+import About from '../components/Fellowship/About';
+import Faq from '../components/Fellowship/Faq';
 const FellowshipPage = () => {
     return (
         <>
+            <About />
             <Provide />
+            <Faq />
             <Sponsor />
         </>
     );
